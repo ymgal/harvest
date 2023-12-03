0fe0fd9018a1b66f97388fe72aa6aba100522e68
sorakylin <pyb068@gmail.com>
2023-12-03 21:20:21 +0800

    Supplementary test.

---
+++ src/test/java/com/ymgal/harvest/HarvestTests.java
@@ -0,0 +1,17 @@
+package com.ymgal.harvest;
+
+
+public class HarvestTests {
+
+    public static void main(String[] args) throws Exception {
+        //clannad
+        Harvest harvest = new VndbHarvest("https://vndb.org/v17");
+        harvest.get().thenApply(r -> {
+            ValidateUtil.validate(r);
+            return r;
+        }).thenAccept(System.out::println).join();
+
+//        HarvestResult vndbHarvest = harvest.exec("https://vndb.org/v4", new InetSocketAddress(80));
+//
+    }
+}
